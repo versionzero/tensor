ceed05ec241ac818d12785f4559d9cf094bb23bb
Ben Burnett <ben.burnett@gmail.com>
2011-05-09 01:49:20 -0600

    + Added tensor creation primitives

--- tensor_new.cc
+++ tensor_new.cc
@@ -1,62 +1,38 @@
 
 #include "error.h"
-#include "tensor.h"
+#include "memory.h"
 #include "mmio.h"
-
+#include "storage.h"
+#include "tensor.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 tensor_t*
-tensor_new(uint l, uint m, uint n, ownership_t owner)
+tensor_new(uint l, uint m, uint n, uint nnz, storage_strategy_t strategy)
 {
   uint     i, j, k;
   tensor_t *tr;
-  double   *p;
+  void     *p;
   
   if (NULL == (tr = (tensor_t*) malloc(sizeof(tensor_t)))) {
     die("Failed to allocate tensor.\n");
   }
-
-  tr->l     = l;
-  tr->m     = m;
-  tr->n     = n;
-  tr->data  = NULL;
-  tr->owner = owner;
   
-  if (viewer == owner) {
-    return tr;
-  }
-
-  if (NULL == (p = (double*) malloc(l*m*n*sizeof(double)))) {
-    die("Failed to allocate tensor storage.\n");
+  tr->strategy = strategy;
+  p            = NULL;
+  
+  if (0 == nnz ) {
+    switch(strategy) {
+    case cooridinate:
+      p = storage_cooridinate_strategy_new(nnz);
+      break;
+    case ekmr:
+      p = storage_ekmr_strategy_new(nnz);
+      break;
+    }    
   }
 
   tr->storage = p;
 
-  if (NULL == (tr->data = (double***) malloc(l*sizeof(double)))) {
-    die("Failed to allocate tensor slice data layout.\n");
-  }
-  for (i = 0; i < l; ++i) {
-    if (NULL == (tr->data[i] = (double**) malloc(m*sizeof(double)))) {
-      die("Failed to allocate row data layout.\n");
-    }
-    for (j = 0; j < m; ++j) {
-      tr->data[i][j] = p;
-      p += n;
-    }    
-  }
-
-  return tr;
-}
-
-tensor_t*
-tensor_new(uint l, uint m, uint n, ownership_t owner)
-{
-  tensor_t *tr;
-
-  if (NULL == (tr = tensor_new(m, n))) {
-    die("Failed to allocate tensor.\n");
-  }
-  
   return tr;
 }
