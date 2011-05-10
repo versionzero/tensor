d4562ebc5e791a2285dd994a4d367fdcf8b5b493
Ben Burnett <ben.burnett@gmail.com>
2011-05-09 23:30:19 -0600

    + CASTED_MALLOC => MALLOC

--- tensor_storage_new.cc
+++ tensor_storage_new.cc
@@ -1,7 +1,8 @@
 
 #include "error.h"
-#include "tensor.h"
+#include "memory.h"
 #include "storage.h"
+#include "tensor.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,11 +12,11 @@ tensor_storage_coordinate_strategy_new(uint nnz)
 {
   coordinate_storage_t *storage;
   
-  storage         = CASTED_MALLOC(coordinate_storage_t);
-  storage->values = CASTED_MALLOC_N(double, nnz);
-  storage->I      = CASTED_MALLOC_N(uint, nnz);
-  storage->J      = CASTED_MALLOC_N(uint, nnz);
-  storage->K      = CASTED_MALLOC_N(uint, nnz);
+  storage         = MALLOC(coordinate_storage_t);
+  storage->values = MALLOC_N(double, nnz);
+  storage->I      = MALLOC_N(uint, nnz);
+  storage->J      = MALLOC_N(uint, nnz);
+  storage->K      = MALLOC_N(uint, nnz);
   
   return storage;
 }
@@ -26,60 +27,11 @@ tensor_storage_ekmr_strategy_new(uing nnz)
   ekmr_storage_t *storage;
   
 #if 0
-  storage         = CASTED_MALLOC(ekmr_storage_t);
-  storage->values = CASTED_MALLOC_N(double, nnz);
-  storage->I      = CASTED_MALLOC_N(uint, nnz);
-  storage->J      = CASTED_MALLOC_N(uint, nnz);  
+  storage         = MALLOC(ekmr_storage_t);
+  storage->values = MALLOC_N(double, nnz);
+  storage->I      = MALLOC_N(uint, nnz);
+  storage->J      = MALLOC_N(uint, nnz);  
 #endif
 
   return storage;
-}
-
-tensor_t*
-tensor_new(uint l, uint m, uint n, uint nnz, storage_strategy_t s, ownership_t o)
-{
-  uint     i, j, k;
-  tensor_t *tr;
-  void     *p;
-  
-  tr           = (tensor_t*) malloc_or_die(sizeof(tensor_t));
-  tr->l        = l;
-  tr->m        = m;
-  tr->n        = n;
-  tr->owner    = o;
-  tr->strategy = s;
-  tr->storage  = NULL;  
-  
-  if (viewer == owner) {
-    return tr;
-  }
-
-  switch(s) {
-  case coordinate:
-    p = tensor_new_coordinate_storage(nnz);
-    break;
-  case ekmr:
-    p = tensor_new_ekmr_storage(nnz);
-    break;
-  }
-
-  tr->storage = p;
-
-  return tr;
-}
-
-tensor_t*
-tensor_new(uint l, uint m, uint n, ownership_t o)
-{
-  tensor_t *tr;
-
-  tr           = (tensor_t*) malloc_or_die(sizeof(tensor_t));
-  tr->l        = l;
-  tr->m        = m;
-  tr->n        = n;
-  tr->owner    = o;
-  tr->strategy = none;
-  tr->storage  = NULL;
-  
-  return tr;
 }
