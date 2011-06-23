75bd8cc1d43f48857b6387916efeb01d9c49936f
Ben Burnett <ben.burnett@gmail.com>
2011-06-23 12:24:31 -0600

    + New annotation script and the resulting headers

--- annotate.sh
+++ annotate.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -x
+#!/bin/sh
 
 declare -a strategies=('compressed' 'ekmr' 'zzekmr')
 for strategy in ${strategies[@]}; do
-    cg_annotate --threshold=100 --auto=yes cache.${strategy}.dat >| annotate.${strategy}.h
+    cg_annotate --threshold=20 --auto=yes cache.${strategy}.dat >| annotate.${strategy}.h
 done
