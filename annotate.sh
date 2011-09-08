3081acdbc6fd89bf7efcc5d006c499bd118bbee9
Ben Burnett <ben.burnett@gmail.com>
2011-09-08 14:22:10 -0600

    + Removed some unused scripts + Updated the simple tracing script

--- annotate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-declare -a strategies=('compressed' 'ekmr' 'zzekmr')
-for strategy in ${strategies[@]}; do
-    cg_annotate --threshold=0 --auto=yes cache.${strategy}.dat >| annotate.${strategy}.h
-done
--- trace_all.sh
+++ trace_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-declare -a STRATEGIES=('compressed' 'compressed' 'slice' 'slice' 'slice')
-declare -a ORIENTATIONS=('row' 'tube' 'lateral' 'horizontal' 'frontal')
+VECTOR=${1:-vector4}
+TENSOR=${2:-example4}
+LINE=${3:-64}
+CACHE=${4:-2048}
 
-for i in {0..4}; do
-    ./tensor effectuate -l 8 -m 32 -o n-mode -s input/vector2.in ieee-fig4.${ORIENTATIONS[$i]}.in ieee-fig4.${ORIENTATIONS[$i]}.out
+declare -a STRATEGIES=('compressed' 'compressed' 'slice' 'slice' 'slice' 'ekmr')
+declare -a ORIENTATIONS=('row' 'tube' 'lateral' 'horizontal' 'frontal' 'row')
+
+for i in {0..5}; do
+    NAME=${TENSOR}.${STRATEGIES[$i]}.${ORIENTATIONS[$i]}
+    ./tensor effectuate -l ${LINE} -m ${CACHE} -o n-mode -s ${VECTOR}.in ${NAME}.in ${NAME}.out \
+	>${NAME}.results 2>${NAME}.error
 done
