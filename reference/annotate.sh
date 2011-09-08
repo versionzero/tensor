b8d0acbe367953167fc8990cdd5b1a4dfb9e9551
Ben Burnett <ben.burnett@gmail.com>
2011-09-08 14:06:48 -0600

    + Removed old reference code that is no longer nessesary

--- reference/annotate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for x in {0..2} ; do 
-    cg_annotate cache.${x}.dat --threshold=100 --auto=yes >| annotate.${x}.h
-done
--- reference/cachegrind.sh
+++
@@ -1,5 +0,0 @@
-for x in {0..2} ; do 
-    valgrind --tool=cachegrind --cachegrind-out-file=cache.${x}.dat \
-	--dsymutil=yes  ./tensor -s${x} -o2 input.dat \
-	input.dat output.dat
-done
