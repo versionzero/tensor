2dd3814717fead9e78947e63baa722a30743d47b
Ben Burnett <ben.burnett@gmail.com>
2011-09-23 20:04:23 -0600

    + Vector creation script supports variable length vectors with an identical value in each element

--- scripts/make_vector.sh
+++ scripts/make_vector.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "usage: $0 N"
+if [ $# -le 1 ]; then
+    echo "usage: $0 N M"
     exit 1
 fi
 
 N=$1
+M=${2:--1}
 
 echo "%%MatrixMarket vector array real    general            "
 echo $N
 
 for (( i=1; i<=N; i++ )); do
-    echo $i
+    if [ $M = -1 ]; then
+	echo $i
+    else
+	echo $M
+    fi
 done
