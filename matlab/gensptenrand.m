26f281f20cd63ce7b21f02306468ac0569e41126
Ben Burnett <ben.burnett@gmail.com>
2011-10-18 14:55:31 -0600

    + Matlab script for computing random cubic tensors

--- matlab/gensptenrand.m
+++ matlab/gensptenrand.m
@@ -1,8 +1,8 @@
 
-for i = 500:50:1000
+for i = 100:100:1000
     name = sprintf('./example%d.in', i);
     disp(name);
-    T = sptenrand([i i i], 0.1);
+    T = sptenrand([i i i], 0.01);
     disp('Writing...');
     fd = fopen(name, 'wt');
     tensor_write(fd, T);
