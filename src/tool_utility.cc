a372446b92806ce62f68c963464848204421e98e
Ben Burnett <ben.burnett@gmail.com>
2011-11-09 13:56:32 -0700

    + Removed uneeded tools

--- src/tool_utility.cc
+++ src/tool_utility.cc
@@ -18,10 +18,8 @@ extern verbosity::type_t noisiness;
 extern bool              write_results;
 
 #define TENSOR_DESCRIPTION     "A tool for working with tensors."
-#define CONVERT_DESCRIPTION    "A tool for converting between th-order tensor storage strategies."
 #define GENERATE_DESCRIPTION   "A tool for generating varieties of th-order tensors."
 #define EFFECTUATE_DESCRIPTION "A tool for performing computations on th-order tensors."
-#define PERMUTE_DESCRIPTION    "A tool for permuting the non-zeros of th-order tensors."
 #define VERSION "Version 0.01 (" __DATE__ "), " \
   "Copyright (C) 2011, and GPLv3'd, by Ben Burnett\n" \
   "This is free software; see the source for copying conditions.  There is NO\n" \
@@ -32,19 +30,15 @@ extern bool              write_results;
 static char const *map_tool_to_string[] = { 
   "unknown",
   "tensor",
-  "convert", 
   "generate",
   "effectuate",
-  "permute"
 };
 
 static char const *map_tools_to_description[] = { 
   "unknown",
   TENSOR_DESCRIPTION,
-  CONVERT_DESCRIPTION,
   GENERATE_DESCRIPTION,
   EFFECTUATE_DESCRIPTION,
-  PERMUTE_DESCRIPTION
 };
 
 char const* string_from_tool(tool::type_t tool)
