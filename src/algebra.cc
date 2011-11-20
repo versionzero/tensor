0f76cf764b123e4f5eef412808a94a4c74d6665c
Ben Burnett <ben.burnett@gmail.com>
2011-11-20 15:47:05 -0700

    + Simple matrix multiplication wrapper around BLAS routines

--- src/algebra.cc
+++ src/algebra.cc
@@ -17,3 +17,16 @@ array_inner_product(const int N, const double *X, const int incX, const double *
 {
   return cblas_ddot(N, X, incX, Y, incY);
 }
+
+#if 0
+ml_matrix matrix_multiply(const ml_matrix &A,const ml_matrix &B)
+{
+    ml_matrix C(A.rows(),B.cols());
+    cblas_dgemm(ML_BLAS_STORAGE,CblasNoTrans,CBlasNoTrans,
+                A.rows(),B.cols(),A.rows(),
+                1.0,A.raw_data(),A.data_stride(),
+                B.raw_data(),B.data_stride(),
+                0.0,C.raw_data(),C.data_stride());
+    return C;
+}
+#endif
