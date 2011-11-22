a1460fa7303f6f161a59c0b557bb4fb602631c6d
Ben Burnett <ben.burnett@gmail.com>
2011-11-21 20:52:56 -0700

    + Do output messages trip gdb?

--- src/blas_dgemm.cc
+++ src/blas_dgemm.cc
@@ -23,22 +23,16 @@ cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
     if (TransA == CblasTrans)            TA='T';
     else if ( TransA == CblasConjTrans ) TA='C';
     else if ( TransA == CblasNoTrans )   TA='N';
-    else die("cblas_dgemm: Illegal TransA setting, %d\n", TransA);
     if (TransB == CblasTrans)            TB='T';
     else if ( TransB == CblasConjTrans ) TB='C';
     else if ( TransB == CblasNoTrans )   TB='N';
-    else die("cblas_dgemm: Illegal TransB setting, %d\n", TransB);
   } else if (Order == CblasRowMajor) {
     if(TransA == CblasTrans)             TB='T';
     else if ( TransA == CblasConjTrans ) TB='C';
     else if ( TransA == CblasNoTrans )   TB='N';
-    else die("cblas_dgemm: Illegal TransA setting, %d\n", TransA);
-    if(TransB == CblasTrans) TA='T';
+    if(TransB == CblasTrans)             TA='T';
     else if ( TransB == CblasConjTrans ) TA='C';
     else if ( TransB == CblasNoTrans )   TA='N';
-    else die("cblas_dgemm: Illegal TransB setting, %d\n", TransB);
-  } else {
-    die("cblas_dgemm: Illegal Order setting, %d\n", Order);
   }
   
   dgemm_(&TA, &TB, &M, &N, &K, &alpha, B, &ldb, A, &lda, &beta, C, &ldc);
@@ -271,8 +265,7 @@ dgemm_(char const *transa, char const *transb, int const *m, int const *n, int c
 	info = 13;
     }
     if (info != 0) {
-      die("DGEMM: %d\n", info);
-      return 0;
+      return info;
     }
 
 /*     Quick return if possible. */
