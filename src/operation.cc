c857d6051b507faba7a9832f603bdc0bfabbcd6d
Ben Burnett <ben.burnett@gmail.com>
2011-11-21 19:09:05 -0700

    + Mode-2 product base implementation added

--- src/operation.cc
+++ src/operation.cc
@@ -32,7 +32,7 @@ operation_mode_1_product(matrix_t *matrix, tensor_t const *tensor, vector_t cons
 }
 
 matrix_t*
-operation_mode_1_product(vector_t const *vector, tensor_t const *tensor)
+operation_mode_1_product(tensor_t const *tensor, vector_t const *vector)
 {
   matrix_t *matrix;
   
@@ -45,4 +45,38 @@ operation_mode_1_product(vector_t const *vector, tensor_t const *tensor)
   operation_mode_1_product(matrix, tensor, vector);
   
   return matrix;
+}
+
+void
+operation_mode_2_product(tensor_t *result, tensor_t const *tensor, matrix_t const *matrix)
+{
+  debug("operation_mode_1_product(matrix=0x%x, tensor=0x%x, matrix=0x%x)\n", matrix, tensor, matrix);
+  
+  compatible(matrix, tensor);
+  
+  switch (tensor->strategy) {
+  case strategy::array:
+    operation_mode_2_product_array(result, tensor, matrix);
+    break;
+  default:
+    die("operation_mode_2_product: tensor product for '%s' strategy (using threads) is not currently supported.\n",
+	strategy_to_string(tensor->strategy));
+    break;
+  }
+}
+
+tensor_t*
+operation_mode_2_product(tensor_t const *tensor, matrix_t const *matrix)
+{
+  tensor_t *result;
+  
+  compatible(matrix, tensor);
+  debug("operation_mode_2_product(tensor=0x%x, matrix=0x%x)\n", tensor, matrix);
+  
+  result = tensor_malloc(tensor->l, tensor->m, tensor->n, ownership::creator);
+  debug("operation_mode_2_product: result=0x%x\n", result);
+ 
+  operation_mode_2_product(result, tensor, matrix);
+  
+  return result;
 }
--- src/operation_mode_2_product.cc
+++ src/operation_mode_2_product.cc
@@ -10,7 +10,6 @@
 #include "thread.h"
 #include "tensor.h"
 #include "utility.h"
-#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,7 +24,7 @@ extern data::partition::type_t data_partition;
   Computing ($pT$):
   Let $\T \in R^{n\times n\times n}$ be a tensor.
   Let $\M \in R^{n\times n}$ be a matrix.
-  Let $p \in R^{n}$ be a vector.
+  Let $p \in R^{n}$ be a matrix.
   for i = 1 to l do
     for j = 1 to m do 
       for k = 1 to m do
@@ -37,83 +36,14 @@ extern data::partition::type_t data_partition;
 
 typedef struct {
   uint           done;
-  matrix_t       *matrix;
-  vector_t const *vector;
+  tensor_t       *result;
+  matrix_t const *matrix;
   tensor_t const *tensor;
 } product_thread_data_t;
 
-typedef void (*mode_1_product_t)(product_thread_data_t *data, uint n, double **M, double *P, double *T);
+typedef void (*mode_2_product_t)(product_thread_data_t *data, uint n, double *R, double *M, double *T);
 
-int
-fiber_next(product_thread_data_t *data)
-{
-  volatile uint k;
-  
-  /* rather than a lock we can take advantage of the architecture and
-     issue an atomic fetch and increment */
-  k = __sync_fetch_and_add(&data->done, 1);
-  return k < (data->tensor->n*data->tensor->n) ? k : -1;
-}
-
-void
-fiber_consumer_implementation(product_thread_data_t *data, uint n, double **M, double *P, double *T)
-{
-  int  t;
-  uint i, j, offset;
-  
-  while (-1 != (t = fiber_next(data))) {
-    offset  = t*n;
-    i       = t/n;
-    j       = t%n;
-    M[i][j] = array_inner_product(n, P, 1, T+offset, 1);
-  }
-}
-
-thread_address_t
-fiber_consumer(thread_argument_t *argument)
-{
-  product_thread_data_t *data;
-  
-  data = (product_thread_data_t*) thread_data(argument);
-  
-  fiber_consumer_implementation(data, data->tensor->n, data->matrix->data, data->vector->data, data->tensor->values);
-  
-  return NULL;
-}
-
-void
-fiber_block_consumer_implementation(product_thread_data_t *data, uint n, uint start, uint end, double **M, double *P, double *T)
-{
-  uint i, j, t, offset;
-  
-  for (t = start; t < end; ++t) {
-    offset  = t*n;
-    i       = t/n;
-    j       = t%n;
-    M[i][j] = array_inner_product(n, P, 1, T+offset, 1);
-  }
-}
-
-thread_address_t
-fiber_block_consumer(thread_argument_t *argument)
-{
-  int                   id;
-  uint                  n, block_size;
-  uint                  start, end;
-  product_thread_data_t *data;
-  
-  data       = (product_thread_data_t*) thread_data(argument);
-  n          = data->tensor->n;
-  id         = thread_myid(argument);
-  block_size = (n*n)/thread_count;
-  start      = block_size*id;
-  end        = start+block_size;
-  
-  fiber_block_consumer_implementation(data, n, start, end, data->matrix->data, data->vector->data, data->tensor->values);
-  
-  return NULL;
-}
-
+static
 int
 slice_next(product_thread_data_t *data)
 {
@@ -125,22 +55,20 @@ slice_next(product_thread_data_t *data)
   return k < data->tensor->n ? k : -1;
 }
 
+static
 void
-slice_consumer_implementation(product_thread_data_t *data, uint n, double **M, double *P, double *T)
+slice_consumer_implementation(product_thread_data_t *data, uint n, double *R, double *M, double *T)
 {
   int  i;
-  uint j, ioffset, joffset;
+  uint offset;
   
   while (-1 != (i = slice_next(data))) {
-    ioffset = i*n*n;
-    joffset = ioffset;
-    for (j = 0; j < n; ++j) {
-      M[i][j]  = array_inner_product(n, P, 1, T+joffset, 1);
-      joffset += n;
-    }
+    offset = i*n*n;
+    array_matrix_multiply(n, n, n, M, n, T+offset, n, R+offset, n);
   }
 }
 
+static
 thread_address_t
 slice_consumer(thread_argument_t *argument)
 {
@@ -148,56 +76,20 @@ slice_consumer(thread_argument_t *argument)
   
   data = (product_thread_data_t*) thread_data(argument);
   
-  slice_consumer_implementation(data, data->tensor->n, data->matrix->data, data->vector->data, data->tensor->values);
+  slice_consumer_implementation(data, data->tensor->n, data->result->values, data->matrix->data[0], data->tensor->values);
   
   return NULL;
 }
 
-void
-slice_block_consumer_implementation(int id, product_thread_data_t *data, uint n, uint start, uint end, double **M, double *P, double *T)
-{
-  uint i, j, ioffset, joffset;
-  
-  for (i = start; i < end; ++i) {
-    ioffset = i*n*n;
-    joffset = ioffset;
-    for (j = 0; j < n; ++j) {
-      M[i][j]  = array_inner_product(n, P, 1, T+joffset, 1);
-      joffset += n;
-    }
-  }
-}
-
-thread_address_t
-slice_block_consumer(thread_argument_t *argument)
-{
-  int                   id;
-  uint                  n, block_size;
-  uint                  start, end;
-  product_thread_data_t *data;
-  
-  data       = (product_thread_data_t*) thread_data(argument);
-  n          = data->tensor->n;
-  id         = thread_myid(argument);
-  block_size = n/thread_count;
-  start      = block_size*id;
-  end        = start+block_size;
-  
-  DEBUG("thread:%d: block_size=%d/%d=%d, start=%d, end=%d\n", id, n, thread_count, block_size, start, end);
-  
-  slice_block_consumer_implementation(id, data, n, start, end, data->matrix->data, data->vector->data, data->tensor->values);
-  
-  return NULL;
-}
 
 void
-threaded_mode_1_product_array(matrix_t *matrix, vector_t const *vector, tensor_t const *tensor, thread_function_t producer, thread_function_t consumer)
+operation_mode_2_product_array(tensor_t *result, tensor_t const *tensor, matrix_t const *matrix, thread_function_t producer, thread_function_t consumer)
 {
   product_thread_data_t data;
   
   data.done   = 0;
   data.matrix = matrix;
-  data.vector = vector;
+  data.result = result;
   data.tensor = tensor;
   
   if (NULL != producer) {
@@ -208,7 +100,7 @@ threaded_mode_1_product_array(matrix_t *matrix, vector_t const *vector, tensor_t
 }
 
 void
-threaded_mode_1_product_array(matrix_t *matrix, vector_t const *vector, tensor_t const *tensor)
+operation_mode_2_product_array(tensor_t *result, tensor_t const *tensor, matrix_t const *matrix)
 {
   thread_function_t consumer, producer;
   
@@ -216,50 +108,20 @@ threaded_mode_1_product_array(matrix_t *matrix, vector_t const *vector, tensor_t
   consumer = NULL;
   
   switch (data_partition) {
-  case data::partition::fiber:
-    consumer = (thread_function_t) &fiber_consumer;
-    break;
-  case data::partition::fiber_block:
-    consumer = (thread_function_t) &fiber_block_consumer;
-    break;
   case data::partition::slice:
     consumer = (thread_function_t) &slice_consumer;
     break;
-  case data::partition::slice_block:
-    consumer = (thread_function_t) &slice_block_consumer;
+#if 0
+  case data::partition::slice_recursive_block:
+    consumer = (thread_function_t) &slice_recursive_block_consumer;
     break;
+#endif
   default:
-    die("threaded_mode_1_product_array: tensor product for '%s' partition is not currently supported.\n",
+    die("operation_mode_2_product_array: tensor product for '%s' partition is not currently supported.\n",
 	data_partition_to_string(data_partition));
     break;
   }
   
-  threaded_mode_1_product_array(matrix, vector, tensor, producer, consumer);
+  operation_mode_2_product_array(result, tensor, matrix, producer, consumer);
 }
  
-void
-serial_mode_1_product_array(matrix_t *matrix, vector_t const *vector, tensor_t const *tensor)
-{
-  uint   i, j, k;
-  uint   index, sum;
-  uint   n;
-  double **M, *T, *P;
-  
-  debug("mode_1_product_array(matrix=0x%x, vector=0x%x, tensor=0x%x)\n", matrix, vector, tensor);
-  
-  n = tensor->n;
-  M = matrix->data;
-  P = vector->data;
-  T = tensor->values;
-  
-  for (i = 0; i < n; ++i) {
-    for (j = 0; j < n; ++j) {
-      sum = 0;
-      for (k = 0; k < n; ++k) {
-	index = tensor_index(tensor, i, j, k);
-	sum += P[k] * T[index];
-      }
-      M[i][j] = sum;
-    }
-  }
-}
